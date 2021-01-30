b21533ab2f6d6dc17e72976028662feace68e339
David Huecker <dh001@ucr.edu>
2021-01-30 14:54:20 -0800

    Starting C# Random number game

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Project:RandomNumberGame
+//Developer: David Huecker
+//Verision: 1.0
+
+namespace RandomNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to my random number guessing game!");
+        }
+    }
+}
