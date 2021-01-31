1bd3b9e82b69c6fb089b63c96d1eb4bd785516f7
David Huecker <dh001@ucr.edu>
2021-01-31 15:20:14 -0800

    simple but running version of game

--- Program.cs
+++ Program.cs
@@ -1,16 +1,128 @@
-﻿using System;
+﻿//Author: David Huecker
+//Version:1.0
+//Try to select the random that the computer has chosen
+   
+using System;
 
-//Project:RandomNumberGame
-//Developer: David Huecker
-//Verision: 1.0
-
+// Namespace
 namespace RandomNumberGame
 {
+    // Main Class
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello welcome to my random number guessing game!");
+            GetAppInfo(); // Run GetAppInfo function to get info
+
+            GreetUser(); // Ask for users name and greet
+
+            while (true)
+            {
+
+                // Init correct number
+                //int correctNumber = 7;
+
+                // Create a new Random object
+                Random random = new Random();
+
+                // Init correct number
+                int correctNumber = random.Next(1, 10);
+
+                // Init guess var
+                int guess = 0;
+
+                // Ask user for number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                // While guess is not correct
+                while (guess != correctNumber)
+                {
+                    // Get users input
+                    string input = Console.ReadLine();
+
+                    // Make sure its a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        // Print error message
+                        PrintColorMessage(ConsoleColor.Red, "Please use an actual number");
+
+                        // Keep going
+                        continue;
+                    }
+
+                    // Cast to int and put in guess
+                    guess = Int32.Parse(input);
+
+                    // Match guess to correct number
+                    if (guess != correctNumber)
+                    {
+                        // Print error message
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+                    }
+                }
+
+                // Print success message
+                PrintColorMessage(ConsoleColor.Yellow, "CORRECT!");
+
+                // Ask to play again
+                Console.WriteLine("Play Again? [Y or N]");
+
+                // Get answer
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y") {
+                    continue;
+                }
+                else if (answer == "N") {
+                     Console.WriteLine("Thanks for playing! Goodbye");
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }           
+
+        }
+
+        // Get and display app info
+        static void GetAppInfo() {
+            // Set app vars
+            string appName = "Random Number Game";
+            string appVersion = "1.0.0";
+            string appAuthor = "David Huecker";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset text color
+            Console.ResetColor();
+        }
+
+        // Ask users name and greet
+        static void GreetUser() {
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+        }
+
+        // Print color message
+        static void PrintColorMessage(ConsoleColor color, string message) {
+            // Change text color
+            Console.ForegroundColor = color;
+
+            // Tell user its not a number
+            Console.WriteLine(message);
+
+            // Reset text color
+            Console.ResetColor();
         }
     }
 }
